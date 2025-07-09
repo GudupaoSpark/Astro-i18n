328190a9f2b8b91f20a513e5f393cc68117c5e51
dongguacute <gudupao2022@gmail.com>
2025-07-09 12:47:09 +0800

    修不好

--- test/astro.config.mjs
+++ test/astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import { astroI18nPlugin } from '../src/index.ts';
+import { astroI18nPlugin } from '../src/plugin.ts';
 // https://astro.build/config
 export default defineConfig({
     integrations: [
