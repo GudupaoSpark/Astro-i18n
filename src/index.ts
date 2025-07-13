b591c315f64fa97c5ec3f00796bbf15bf63424b1
lidongxun967 <1985409711@qq.com>
2025-07-14 04:51:01 +0800

    refactor(src): 将 createClientTranslator 从 client.js 导出
    - 之前遗漏了 createClientTranslator 的导出
    - 此更改使 createClientTranslator 可以在其他模块中使用

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { astroI18nPlugin } from './plugin.js';
 export { getTranslator, getStaticPaths,getTranslations } from './translator.js';
 export type { LocaleMap, Translations, I18nHelper, Locals, AstroI18nOptions } from './types.js';
-import { createClientTranslator } from './client.js';
+export { createClientTranslator } from './client.js';
