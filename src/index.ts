45d3c76eded8cfb20a71ed06e755cb87272ad75b
dongguacute <gudupao2022@gmail.com>
2025-08-28 17:02:44 +0800

    feat(translator): export getAvailableLanguages function
    - Add getAvailableLanguages to the list of exported functions in src/index.ts
    - This change enables users to access the getAvailableLanguages function from the main package entry point

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { astroI18nPlugin } from './plugin.js';
-export { getTranslator, getStaticPaths,getTranslations } from './translator.js';
+export { getTranslator, getStaticPaths,getTranslations ,getAvailableLanguages} from './translator.js';
 export type { LocaleMap, Translations, I18nHelper, Locals, AstroI18nOptions } from './types.js';
 export { createClientTranslator } from './client.js';
